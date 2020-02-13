94c314686b4b8587c8ea4cb0d27e586d429a0db1
CCulver831 <c.culver831@gmail.com>
2020-02-13 00:09:05 -0800

    Fixed NPC spawning. Wave system yet to be fully implemented. Will add GOAP and power ups soon

---
+++ HighNoonSimulator/Assets/Scripts/Spawn/Spawning.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+
+    //Setting up array of enemies
+    public GameObject[] Enemies;
+    public GameObject[] spawnLoc;
+    private int spawnAmount = 0;
+    public int total; 
+    public float SpawnRate = 2.0f;
+    private int wave = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+
+    //Spawn Enemies on increment
+    IEnumerator Spawn()
+    {
+        Debug.Log("enemies Spawning!");
+        while(spawnAmount < total)
+        {
+            int s = Random.RandomRange(0, 3);
+            int e = Random.RandomRange(0, 6);
+            Instantiate(Enemies[e], new Vector3(spawnLoc[s].transform.position.x, spawnLoc[s].transform.position.y, spawnLoc[s].transform.position.z), Quaternion.identity);
+            yield return new WaitForSeconds(SpawnRate);
+            spawnAmount++;
+        }
+        SpawnRate = SpawnRate / 0.5f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
