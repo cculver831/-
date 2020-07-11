0d1b523e94c515360517ce817b65c3f90426b0eb
CCulver831 <c.culver831@gmail.com>
2020-07-10 22:57:13 -0700

    Debugging ammao drops

--- HighNoonSimulator/Assets/Scripts/Pickups/AmmoPickup.cs
+++ HighNoonSimulator/Assets/Scripts/Pickups/AmmoPickup.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class AmmoPickup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-     
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         
-       transform.Rotate(0, 50 * Time.deltaTime, 0); //rotates 50 degrees per second around z axis
+       //transform.Rotate(0, 50 * Time.deltaTime, 0); //rotates 50 degrees per second around z axis
     }
     private void OnTriggerEnter(Collider other)
     {
